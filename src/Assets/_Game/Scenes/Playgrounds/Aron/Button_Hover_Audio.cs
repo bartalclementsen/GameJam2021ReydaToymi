ca38faaf5dcc7cceda15cd412c02e54f603d1fec
AronLenvig <Aronlenvig@hotmail.com>
2021-09-12 15:13:50 +0100

    main manu

---
+++ src/Assets/_Game/Scenes/Playgrounds/Aron/Button_Hover_Audio.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Button_Hover_Audio : MonoBehaviour
+{
+    void OnMouseEnter()
+    {
+      Debug.Log("Halloo");
+    }
+  
+}
